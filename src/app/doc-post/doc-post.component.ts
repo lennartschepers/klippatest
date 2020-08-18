58bb5fdd1fe4281f99e9f0d2bf395acb05c57ef3
lennartschepers <lenschepers@gmail.com>
2020-08-18 16:06:16 +0200

    partial table and image support

--- src/app/doc-post/doc-post.component.ts
+++ src/app/doc-post/doc-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-doc-post',
@@ -12,25 +13,44 @@ export class DocPostComponent implements OnInit {
     url: 'https://paperreceipts.org/wp-content/uploads/2019/11/receipt-2.png',
 }
   end= `https://custom-ocr.klippa.com/api/v1/parseDocument?X-Auth-Key=xwGuqWSo1IwfnfcZ4JBS8Cno8upFCSJW`;
-  json;
+  response;
   fileToUpload;
-  
+  url; 
 
   postMethod(files: FileList) {
     this.fileToUpload = files.item(0); 
+    
     let formData = new FormData(); 
     formData.append('document', this.fileToUpload, this.fileToUpload.name); 
-    this.http.post("https://custom-ocr.klippa.com/api/v1/parseDocument?X-Auth-Key=xwGuqWSo1IwfnfcZ4JBS8Cno8upFCSJW", formData).subscribe((val) => {
-
-    console.log(val);
-    });
-    return false; 
+    this.http.post("https://custom-ocr.klippa.com/api/v1/parseDocument?X-Auth-Key=xwGuqWSo1IwfnfcZ4JBS8Cno8upFCSJW", formData).subscribe((val:any) => {
+    console.log(val.data)
+    var reader = new FileReader();
+    reader.readAsDataURL(this.fileToUpload);
+    reader.onload = (_event) => {
+      this.url = reader.result;
     }
+    console.log('adfad' + this.url)
+    return this.response = val.data;
+    });
+    }
+
+  imagePreview() {
+    console.log('adf');
+  }
+
+
+
+  // tableMaker(response) {
+//    let reponseArray =  response.entries()
+    
+  //}
 
   constructor(private http:HttpClient) { }
   ngOnInit() {
-    this.http.post(this.end,this.postData).toPromise().then((data: any) => {
-      console.log(data);
-      this.json = JSON.stringify(data.data);
-    });
+   // this.http.post(this.end,this.postData).toPromise().then((data: any) => {
+      // console.log(data);
+     // this.json = JSON.stringify(data.data);
+    //});
+    
+    
   }}
